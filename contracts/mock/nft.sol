1ef0b5294b4f19f285e3f07c4ab9c1335206cbcd
kasodesyn <bokang.qian@desyn.io>
2023-01-03 10:13:20 +0800

    rename nft project name

--- contracts/mock/nft.sol
+++ contracts/mock/nft.sol
@@ -9,22 +9,18 @@ contract NFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("GameItem", "ITM") {
+    constructor() ERC721("Giant Cat", "GC") {
         _tokenIds.increment();
-        mint(msg.sender, "just for mock nft");
-        mint(msg.sender, "just for mock nft");
-        mint(msg.sender, "just for mock nft");
-        mint(msg.sender, "just for mock nft");
-        mint(msg.sender, "just for mock nft");
+        mint(msg.sender);
+        mint(msg.sender);
+        mint(msg.sender);
+        mint(msg.sender);
+        mint(msg.sender);
     }
 
-    function mint(address player, string memory tokenURI)
-        public
-        returns (uint256)
-    {
+    function mint(address player) public returns (uint256) {
         uint256 newItemId = _tokenIds.current();
         _mint(player, newItemId);
-        _setTokenURI(newItemId, tokenURI);
 
         _tokenIds.increment();
         return newItemId;
